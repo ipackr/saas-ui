2ddef693ca0185791243b43e307aa000495f8eb9
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-10-05 21:44:15 +0200

    SAAS-642 remove unused file

--- src/pages/ManageOrganization/ManageOrganization.provider.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { ManageOrganizationContext } from './ManageOrganization.types';
-
-export const ManageOrganizationProvider = createContext<ManageOrganizationContext>(
-  {} as ManageOrganizationContext,
-);
