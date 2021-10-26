463097eeb452ed9194c55f33d9520691abf22d16
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-10-26 11:37:57 +0200

    SAAS-642 update messages

--- src/pages/ManageOrganization/InviteMember/InviteMember.messages.ts
+++ src/pages/ManageOrganization/InviteMember/InviteMember.messages.ts
@@ -3,7 +3,7 @@ export const Messages = {
   email: 'Email',
   fetchError: 'An error occurred while fetching members, please try again.',
   inviteMember: 'Invite Member',
-  inviteSuccess: 'User invited successfully',
+  inviteSuccess: 'The user has been successfully invited',
   role: 'Role',
   save: 'Save',
   technicalUser: 'Technical User',
