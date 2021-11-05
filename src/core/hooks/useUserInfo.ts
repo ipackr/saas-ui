0c9125f661e0bbbaccefd1015029d780581de389
Fabio Silva <ffjs1993@gmail.com>
2021-11-05 15:46:10 +0000

    dispatch error if user info fails

--- src/core/hooks/useUserInfo.ts
+++ src/core/hooks/useUserInfo.ts
@@ -2,6 +2,7 @@ import { useCallback, useEffect } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AuthState, UpdateProfilePayload } from 'store/types';
+import { RequestError } from 'core/api/types';
 import { authGetProfileAction, authUpdateProfileAction, getAuth } from '../../store/auth';
 
 export const useUserInfo = (): [AuthState, (payload: UpdateProfilePayload) => void] => {
@@ -19,13 +20,18 @@ export const useUserInfo = (): [AuthState, (payload: UpdateProfilePayload) => vo
 
       if (freshData) {
         dispatch(authGetProfileAction.request());
-        const { email: oktaEmail, family_name, given_name } = await oktaAuth.getUser();
 
-        dispatch(authGetProfileAction.success({
-          email: oktaEmail,
-          firstName: given_name,
-          lastName: family_name,
-        }));
+        try {
+          const { email: oktaEmail, family_name, given_name } = await oktaAuth.getUser();
+ 
+          dispatch(authGetProfileAction.success({
+            email: oktaEmail,
+            firstName: given_name,
+            lastName: family_name,
+          }));
+        } catch (e) {
+          dispatch(authGetProfileAction.failure(e as RequestError));
+        }
       }
     };
 
